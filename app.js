f42d1111b13a4b5877a1ffdc80795c015f2ef674
Salar Qureshi <salarqureshi.1996@gmail.com>
2019-04-21 22:44:02 +0500

    Add api route for homepage recommendation

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.use('/recommendation', require('./routes/api/recommendation'));
 
 app.use('/ubrecommendation', require('./routes/api/ubrecommendation'));
 
+app.use('/ubrecommendation2', require('./routes/api/ubrecommendation2'));
+
 app.listen('3000', () => {
     console.log('Server started on port 3000');
 });
