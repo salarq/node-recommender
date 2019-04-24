924efb1be5ce5093cca5737315390bcf9ccc9969
Salar Qureshi <salarqureshi.1996@gmail.com>
2019-04-24 20:52:59 +0500

    FIlter JSON data

--- routes/api/ubrecommendation2.js
+++ routes/api/ubrecommendation2.js
@@ -38,7 +38,7 @@ router.get('/:id', (req, res) => {
                 var hamzaNeeds = [];
                 for (const item of recommendations.recommendations) {
                     var adr = item.thing;
-                    var sql = 'SELECT DISTINCT products.product_id, product_shop.ps_id, products.product_name, product_shop.product_link, product_shop.product_image, product_shop.product_views, product_shop.product_rating, product_shop.product_price, shop.shop_name, brands.brand_name FROM products INNER JOIN product_shop ON Products.product_id = product_shop.product_id INNER JOIN shop ON product_shop.shop_id = shop.shop_id inner join brands on brands.brand_id = products.brand_id WHERE products.product_id = ? limit 1';
+                    var sql = 'SELECT DISTINCT products.product_id, product_shop.ps_id, products.product_name, product_shop.product_link, product_shop.product_feature_RAM, product_shop.product_feature_ROM, product_shop.product_feature_screenSize, product_shop.product_feature_processor, product_shop.product_image, product_shop.product_views, product_shop.product_rating, product_shop.product_price, shop.shop_name, brands.brand_name FROM products INNER JOIN product_shop ON Products.product_id = product_shop.product_id INNER JOIN shop ON product_shop.shop_id = shop.shop_id inner join brands on brands.brand_id = products.brand_id WHERE products.product_id = ? limit 1';
                 // db.query('products.product_id, product_shop.ps_id, products.product_name,product_shop.product_link, product_shop.product_image, product_shop.product_rating, product_shop.product_price, shop.shop_name, shop.shop_id, brands.brand_name;', similarDocuments.id , (err,results) => {
                     const [rowws, fields] = await promisePool.query(sql, adr);
                     hamzaNeeds.push({
@@ -46,6 +46,10 @@ router.get('/:id', (req, res) => {
                         ps_id: rowws[0].ps_id,
                         product_name: rowws[0].product_name,
                         product_link: rowws[0].product_link,
+                        product_feature_RAM: rowws[0].product_feature_RAM,
+                        product_feature_ROM: rowws[0].product_feature_ROM,
+                        product_feature_screenSize: rowws[0].product_feature_screenSize,
+                        product_feature_processor: rowws[0].product_feature_processor,
                         product_image: rowws[0].product_image,
                         product_views: rowws[0].product_views,
                         product_rating: rowws[0].product_rating,
